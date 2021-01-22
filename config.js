fbb380c0c52f1657ac7abb4522c35830d29e1103
Acee R Charleston <archarle@alumni.iu.edu>
2021-01-22 01:26:16 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '887750832057974',
+        clientSecret: '6f49357110d4aff44a9f7881a85d4995'
+    }
 }
